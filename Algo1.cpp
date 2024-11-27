8043bc74bc6a513c99325c307a6561be176dead0
Lexure Velasco <lexure.velasco@tuhh.de>
2024-11-27 18:09:46 +0100

    finished opt_tilde function of lemma 13

--- Algo1.cpp
+++ Algo1.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 
-vector<double> opt_tilde(int n, int t, vector<int> weight, vector<int> profit) {
+double opt_tilde(int n, double t, vector<double> weight, vector<double> profit) {
     
-    vector<double> profit_weight_ratio(n);
-
-    for(int i=0; i<n; i++){
-        profit_weight_ratio[i] = profit[i] / weight[i];
+    
+    vector<pair<double, int>> profit_weight_ratios(n);
+    for (int i = 0; i < n; ++i) {
+        profit_weight_ratios[i] = {profit[i] / weight[i], i};  // Store ratio and original index
     }
 
-    return profit_weight_ratio;
+    sort(profit_weight_ratios.begin(), profit_weight_ratios.end(), greater<pair<double, int>>());
+
+    double total_profit = 0.0;
+    double total_weight = 0.0;  
+    for (int i = 0; i < n && total_weight + weight[profit_weight_ratios[i].second] <= t; ++i) {
+        int index = profit_weight_ratios[i].second;  
+        total_weight += weight[index];  
+        total_profit += profit[index];  
+    }
+
+    return total_profit;
 }
 
 int main() {
 
-    cout<<"Does this work \n";
-
-    vector<double> ratio = opt_tilde(4, 7, {1,2,3,4}, {1,2,3,4});
-    cout<<"Does this work2 \n";
-    for(int i=0; i<ratio.size();i++){
-        cout << ratio[i] << endl;
-    }
     
+    int n = 4;    //number of items
+    double t = 7; //weight
+    double result= opt_tilde(n, t, {1,2,3,4}, {1,1,1,1});
+    cout<< "t = " << t << endl;
+    cout<< "opt_tilde = " << result <<endl;
 
-    cout<<"Why not printing";
     
     return 0;   
 }
