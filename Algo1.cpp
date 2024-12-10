d6a1e60a03c221bf033151c2cc68e90f14bbf950
Lexure Velasco <lexure.velasco@tuhh.de>
2024-12-10 16:36:42 +0100

    adjust the functions to item struct

--- Algo1.cpp
+++ Algo1.cpp
@@ -3,25 +3,66 @@
 #include <algorithm>
 using namespace std;
 
+struct Item {
+    int value;
+    int weight;
+};
 
-double opt_tilde(int n, double t, vector<double> weight, vector<double> profit) {
-    
-    
-    vector<pair<double, int>> profit_weight_ratios(n);
-    for (int i = 0; i < n; i++) {
-        profit_weight_ratios[i] = {profit[i] / weight[i], i};  //store ratio and original index
+ostream& operator<<(ostream &os, const Item &item) {
+    os << "Value: " << item.value << ", Weight: " << item.weight;
+    return os;
+}
+
+int getMaxWeight(const vector<Item>& items) {
+    if (items.empty()) {
+        return -1;  
     }
 
-    sort(profit_weight_ratios.begin(), profit_weight_ratios.end(), greater<pair<double, int>>());
+    int maxWeight = items[0].weight;  
 
-    double total_profit = 0.0;
+    for (const auto& item : items) {
+        if (item.weight > maxWeight) {
+            maxWeight = item.weight; 
+        }
+    }
+
+    return maxWeight;
+}
+
+int getMaxValue(const vector<Item>& items) {
+    if (items.empty()) {
+        return -1; 
+    }
+
+    int maxValue = items[0].value; 
+
+    for (const auto& item : items) {
+        if (item.value > maxValue) {
+            maxValue = item.value;  
+        }
+    }
+
+    return maxValue;
+}
+
+double opt_tilde(int n, double t, vector<Item> items) {
+    
+    
+    vector<pair<double, int>> value_weight_ratios(n);
+    for (int i = 0; i < n; i++) {
+        value_weight_ratios[i] = {static_cast<double>(items[i].value) / items[i].weight, i};  //store ratio and original index
+    }
+
+    sort(value_weight_ratios.begin(), value_weight_ratios.end(), greater<pair<double, int>>());
+
+    double total_value = 0.0;
     double total_weight = 0.0;  
-    int i; // Use this variable to keep track of the last fully included index
+    int i; 
     for (i = 0; i < n; ++i) {
-        int index = profit_weight_ratios[i].second;
-        if (total_weight + weight[index] <= t) {  // If full item fits, take it completely
-            total_weight += weight[index];
-            total_profit += profit[index];
+        int index = value_weight_ratios[i].second;
+        if (total_weight + items[index].weight <= t) {  // If full item fits, take it completely
+            total_weight += items[index].weight;
+            total_value += items[index].value;
         } else {
             break;  // Stop when capacity is exceeded
         }
@@ -29,18 +70,18 @@ double opt_tilde(int n, double t, vector<double> weight, vector<double> profit) 
 
     // Include the whole (i* + 1)-th item if it exists
     if (i < n) {  // Check if there is an (i* + 1)-th item
-        int index = profit_weight_ratios[i].second;
-        total_profit += profit[index];  // Add the full profit of the (i* + 1)-th item
+        int index = value_weight_ratios[i].second;
+        total_value += items[index].value;  // Add the full value of the (i* + 1)-th item
     }
 
-    return total_profit;
+    return total_value;
 }
 
-int compute_q(int n, double t, vector<double> weight, vector<double> profit) {
-    double w_max = *max_element(weight.begin(), weight.end());
-    double p_max = *max_element(profit.begin(), profit.end());
+int compute_q(int n, double t, vector<Item> items) {
+    double w_max = getMaxWeight(items);
+    double p_max = getMaxValue(items);
 
-    double opt_tild = opt_tilde(n, t, weight, profit);
+    double opt_tild = opt_tilde(n, t, items);
 
     double min_value = min(t/w_max, opt_tild/p_max);
 
@@ -55,14 +96,14 @@ int compute_q(int n, double t, vector<double> weight, vector<double> profit) {
 
 int main() {
 
-    
+    vector<Item> item = {{1,1}, {1,2}, {1,3}, {1,4}};
     int n = 4;    //number of items
     double t = 7; //weight
-    double result= opt_tilde(n, t, {1,2,3,4}, {1,1,1,1});
+    double result= opt_tilde(n, t, item);
     cout<< "t = " << t << endl;
     cout<< "opt_tilde = " << result <<endl;
 
-    int q = compute_q(n, t, {1,2,3,4}, {1,1,1,1});
+    int q = compute_q(n, t, item);
     cout<< "q = " << q << endl;
 
     
